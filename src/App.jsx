e5a63117177aa0770a1b27cf1553424ad98f4eda
Kacper Makiel <90635502+Kacegz@users.noreply.github.com>
2024-10-04 15:23:59 +0200

    Change styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,16 @@ function App() {
   const [tasks, setTasks] = useState(defaultTasks);
   const dialog = useRef();
   const [name, setName] = useState("");
+  const [completed, setCompleted] = useState(false);
 
   function addNewTask() {
+    if (!name) {
+      return;
+    }
     const newTask = {
       id: tasks.length + 1,
       name: name,
-      completed: false,
+      completed: completed,
     };
     setTasks([...tasks, newTask]);
     setName("");
@@ -33,40 +37,77 @@ function App() {
 
   return (
     <>
-      <div className={styles.header}>ToDo App</div>
+      <div className={styles.header}>
+        <h2>ToDo App</h2>
+      </div>
       <div className={styles.main}>
         {tasks.map((task) => (
-          <div key={task.id} className={styles.task}>
+          <div
+            key={task.id}
+            className={`${styles.task} ${
+              task.completed ? styles.finished : ""
+            }`}
+          >
             <span>{task.name}</span>
-            <input
-              type="checkbox"
-              defaultChecked={task.completed}
-              onClick={() => changeTaskStatus(task.id)}
-            />
+            <div className={styles.task_buttons}>
+              <input
+                type="checkbox"
+                defaultChecked={task.completed}
+                className={styles.completed_button}
+                onClick={() => changeTaskStatus(task.id)}
+              />
+              <input
+                type="button"
+                value="delete"
+                className={styles.delete_button}
+              />
+            </div>
           </div>
         ))}
       </div>
       <div className={styles.footer}>
         <button
-          className={styles.addTask}
+          className={styles.new_task_button}
           onClick={() => dialog.current.showModal()}
         >
-          +
+          Add new task
         </button>
       </div>
       <dialog ref={dialog} className={styles.modal}>
         <div className={styles.modal_inputs}>
-          <label htmlFor="name">Enter the name for new task</label>
+          <label htmlFor="name">
+            <h3>Enter the name for a new task</h3>
+          </label>
           <input
             type="text"
             name="name"
             id=""
+            className={styles.modal_input_text}
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+          <label htmlFor="completed">Completed?</label>
+          <input
+            type="checkbox"
+            name="completed"
+            id=""
+            className={styles.modal_checkbox}
+            defaultChecked={completed}
+            onChange={(e) => setCompleted(e.target.checked)}
+          />
           <div className={styles.modal_buttons}>
-            <button onClick={() => addNewTask()}>Add</button>
-            <button onClick={() => dialog.current.close()}>Cancel</button>
+            <button
+              className={styles.modal_button}
+              onClick={() => addNewTask()}
+            >
+              Add
+            </button>
+            <button
+              className={`${styles.modal_button} ${styles.modal_cancel}`}
+              onClick={() => dialog.current.close()}
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </dialog>
