dac53bdc2cdbaeb3b5b3e376d6e7813b5cf00deb
Kacper Makiel <90635502+Kacegz@users.noreply.github.com>
2024-10-11 13:30:17 +0200

    refactor tests

--- spec/Gallery.test.js
+++ spec/Gallery.test.js
@@ -2,28 +2,27 @@ import { render, screen, act } from "@testing-library/react";
 import Gallery from "../src/Gallery/Gallery";
 
 describe("Gallery", () => {
-  it("Renders four images", async () => {
+  beforeEach(() => {
     render(<Gallery />);
+  });
+  it("Renders four images", async () => {
     const div = screen.getByTestId("gallery");
     const images = div.querySelectorAll("img");
     expect(images.length).toBe(4);
   });
   it("Renders the first image", async () => {
-    render(<Gallery />);
     const div = screen.getByTestId("gallery");
     const images = div.querySelectorAll("img");
     expect(images[0]).toBeInTheDocument();
     expect(images[0].alt).toContain("photo1");
   });
   it("Renders the second image", async () => {
-    render(<Gallery />);
     const div = screen.getByTestId("gallery");
     const images = div.querySelectorAll("img");
     expect(images[1]).toBeInTheDocument();
     expect(images[1].src).toContain("european-shorthair");
   });
   it("Renders the third and fourth image, but without a source", async () => {
-    render(<Gallery />);
     const div = screen.getByTestId("gallery");
     const images = div.querySelectorAll("img");
     expect(images[2]).toBeInTheDocument();
--- spec/Utils.test.js
+++ spec/Utils.test.js
@@ -1,4 +1,9 @@
 import { addNewTask, changeTaskStatus, deleteTask } from "../src/utils";
+let tasks = [
+  { id: 1, name: "Buy groceries", completed: false },
+  { id: 2, name: "Walk the dog", completed: false },
+  { id: 3, name: "Do laundry", completed: true },
+];
 describe("Utils", () => {
   it("Logic for creating tasks works", async () => {
     const newtask = addNewTask(4, "New Task", false);
@@ -12,20 +17,10 @@ describe("Utils", () => {
     }).toThrowError("Name is required");
   });
   it("Logic for changing status works", async () => {
-    let tasks = [
-      { id: 1, name: "Buy groceries", completed: false },
-      { id: 2, name: "Walk the dog", completed: false },
-      { id: 3, name: "Do laundry", completed: true },
-    ];
     const newtasks = changeTaskStatus(tasks, 1);
     expect(newtasks[0].completed).toBe(true);
   });
   it("Logic for deleting task works", async () => {
-    let tasks = [
-      { id: 1, name: "Buy groceries", completed: false },
-      { id: 2, name: "Walk the dog", completed: false },
-      { id: 3, name: "Do laundry", completed: true },
-    ];
     const newtasks = deleteTask(tasks, 1);
     expect(newtasks.length).toBe(2);
   });
